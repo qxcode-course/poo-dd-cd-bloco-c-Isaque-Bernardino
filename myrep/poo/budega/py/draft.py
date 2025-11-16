67c6152ce3703182d356da009f8ad2100270e27c
Isaque-Bernardino <135618611+Isaque-Bernardino@users.noreply.github.com>
2025-11-16 13:29:59 +0000

    budega concluida

--- myrep/poo/budega/py/draft.py
+++ myrep/poo/budega/py/draft.py
@@ -13,22 +13,22 @@ class Budega:
         self.espera.append(pessoa)
     def call(self, index: int):
         if index < 0 or index >= len(self.caixas):
-            print("indice inexistente")
+            print("fail: caixa inexistente")
             return
         if self.caixas[index] is not None:
-            print("caixa ocupado")
+            print("fail: caixa ocupado")
             return
         if len(self.espera) == 0:
-            print("ninguem esperando")
+            print("fail: sem clientes")
             return
         self.caixas[index] = self.espera[0]
         del self.espera[0]
     def finish(self, index: int):
         if index < 0 or index >= len(self.caixas):
-            print("indice inexistente")
+            print("fail: caixa inexistente")
             return
         if self.caixas[index] is None:
-            print("caixa vazio")
+            print("fail: caixa vazio")
             return
         self.caixas[index] = None
     def giveUp(self, nome: str) -> Pessoa | None:
@@ -42,3 +42,27 @@ class Budega:
         caixas = ", ".join(["-----" if x is None else str(x) for x in self.caixas])
         espera = ", ".join([str(x) for x in self.espera])
         return f"Caixas: [{caixas}]\nEspera: [{espera}]"
+def main():
+    budega = Budega (0)
+    while True:
+        line: str = input()
+        print("$" + line)
+        args: list[str] = line.split(" ")
+        if args[0] == "end":
+            break
+        elif args[0] == "show":
+            print(budega)
+        elif args[0] == "init":
+            budega = Budega (int(args[1]))
+        elif args[0] == "arrive":
+            cliente = (args[1])
+            budega.enter(Pessoa(cliente))
+        elif args[0] == "call":
+            fila = int(args[1])
+            budega.call(fila)
+        elif args[0] == "finish":
+            finalizar = int(args[1])
+            budega.finish(finalizar)
+        else:
+            print("fail: comando inválido")
+main()
